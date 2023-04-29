24ea974750c32a3e409aea703f1902c5c0479b1d
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2023-04-29 22:51:40 +0200

    Enable dark mode for changelog title bar as well (closes #3)

--- Changelog.cs
+++ Changelog.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,17 @@ namespace Stand_Launchpad
 {
 	public partial class Changelog : Form
 	{
+		[DllImport("dwmapi.dll")]
+		private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
+
 		public Changelog()
 		{
 			InitializeComponent();
+			// dark mode title bar
+			if (DwmSetWindowAttribute(Handle, 19, new[] { 1 }, 4) != 0)
+			{
+				DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
+			}
 		}
 	}
 }
